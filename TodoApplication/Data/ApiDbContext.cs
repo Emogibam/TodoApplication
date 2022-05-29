2c95689a0280db81182bdc68bf291568d0c3b6b1
Ogidan Emmanuel <eogidan22@gmail.com>
2022-05-30 00:01:41 +0100

    first commit

---
+++ TodoApplication/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApplication.Models;
+
+namespace TodoApplication.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public virtual DbSet<ItemData> Items { get; set; }
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
